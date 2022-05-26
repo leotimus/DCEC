fc36c770eb68f24199a63ea3ce04d7ea8aa59d13
Jan-Niklas-Fichte <jan-niklas-fichte@outlook.de>
2022-05-26 11:37:17 +0200

    WIP BatchNormalization instead of zscore

--- vae/VAE_Test.py
+++ vae/VAE_Test.py
@@ -6,11 +6,139 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.python.data import Dataset
-from vamb.__main__ import calc_tnf, calc_rpkm
+from vamb.__main__ import calc_rpkm
 from vamb.vambtools import numpy_inplace_maskarray, zscore, write_npz
 
 from vae.VAE import VAE1
 
+################################# DEFINE FUNCTIONS ##########################
+def log(string, logfile, indent=0):
+    print(('\t' * indent) + string, file=logfile)
+    logfile.flush()
+
+__doc__ = """Calculate tetranucleotide frequency from a FASTA file.
+Usage:
+>>> with open('/path/to/contigs.fna', 'rb') as filehandle
+...     tnfs, contignames, lengths = read_contigs(filehandle)
+"""
+
+import sys as _sys
+import os as _os
+import numpy as _np
+import vamb.vambtools as _vambtools
+
+# This kernel is created in src/create_kernel.py. See that file for explanation
+_KERNEL = _vambtools.read_npz(_os.path.join(_os.path.dirname(_os.path.abspath(__file__)),
+                              "kernel.npz"))
+
+def _project(fourmers, kernel=_KERNEL):
+    "Project fourmers down in dimensionality"
+    s = fourmers.sum(axis=1).reshape(-1, 1)
+    s[s == 0] = 1.0
+    fourmers *= 1/s
+    fourmers += -(1/256)
+    return _np.dot(fourmers, kernel)
+
+def _convert(raw, projected):
+    "Move data from raw PushArray to projected PushArray, converting it."
+    raw_mat = raw.take().reshape(-1, 256)
+    projected_mat = _project(raw_mat)
+    projected.extend(projected_mat.ravel())
+    raw.clear()
+
+def read_contigs(filehandle, minlength=100):
+    """Parses a FASTA file open in binary reading mode.
+    Input:
+        filehandle: Filehandle open in binary mode of a FASTA file
+        minlength: Ignore any references shorter than N bases [100]
+    Outputs:
+        tnfs: An (n_FASTA_entries x 103) matrix of tetranucleotide freq.
+        contignames: A list of contig headers
+        lengths: A Numpy array of contig lengths
+    """
+
+    if minlength < 4:
+        raise ValueError('Minlength must be at least 4, not {}'.format(minlength))
+    print("K")
+    raw = _vambtools.PushArray(_np.float32)
+    projected = _vambtools.PushArray(_np.float32)
+    lengths = _vambtools.PushArray(_np.int)
+    contignames = list()
+
+    entries = _vambtools.byte_iterfasta(filehandle)
+
+    for entry in entries:
+        if len(entry) < minlength:
+            continue
+
+        raw.extend(entry.kmercounts(4))
+
+        if len(raw) > 256000:
+            _convert(raw, projected)
+
+        lengths.append(len(entry))
+        contignames.append(entry.header)
+
+    # Convert rest of contigs
+    _convert(raw, projected)
+    tnfs_arr = projected.take()
+
+    # Don't use reshape since it creates a new array object with shared memory
+    tnfs_arr.shape = (len(tnfs_arr)//103, 103)
+    lengths_arr = lengths.take()
+
+    return tnfs_arr, contignames,
+
+import time
+def calc_tnf(outdir, fastapath, tnfpath, namespath, lengthspath, mincontiglength, logfile):
+    begintime = time.time()
+    log('\nLoading TNF', logfile, 0)
+    log('Minimum sequence length: {}'.format(mincontiglength), logfile, 1)
+
+
+    # If no path to FASTA is given, we load TNF from .npz files
+    if fastapath is None:
+        log('Loading TNF from npz array {}'.format(tnfpath), logfile, 1)
+        tnfs = vamb.vambtools.read_npz(tnfpath)
+        log('Loading contignames from npz array {}'.format(namespath), logfile, 1)
+        contignames = vamb.vambtools.read_npz(namespath)
+        log('Loading contiglengths from npz array {}'.format(lengthspath), logfile, 1)
+        contiglengths = vamb.vambtools.read_npz(lengthspath)
+
+        if not tnfs.dtype == np.float32:
+            raise ValueError('TNFs .npz array must be of float32 dtype')
+
+        if not np.issubdtype(contiglengths.dtype, np.integer):
+            raise ValueError('contig lengths .npz array must be of an integer dtype')
+
+        if not (len(tnfs) == len(contignames) == len(contiglengths)):
+            raise ValueError('Not all of TNFs, names and lengths are same length')
+
+        # Discard any sequence with a length below mincontiglength
+        mask = contiglengths >= mincontiglength
+        tnfs = tnfs[mask]
+        contignames = list(contignames[mask])
+        contiglengths = contiglengths[mask]
+
+    # Else parse FASTA files
+    else:
+        log('Loading data from FASTA file {}'.format(fastapath), logfile, 1)
+    with vamb.vambtools.Reader("/share_data/cami_low/CAMI_low_RL_S001__insert_270_GoldStandardAssembly.fasta", 'rb') as tnffile:
+        ret = vamb.parsecontigs.read_contigs(tnffile, minlength=mincontiglength)
+
+    tnfs, contignames, contiglengths = ret
+    vamb.vambtools.write_npz(os.path.join(outdir, 'tnf.npz'), tnfs)
+    vamb.vambtools.write_npz(os.path.join(outdir, 'lengths.npz'), contiglengths)
+
+    elapsed = round(time.time() - begintime, 2)
+    ncontigs = len(contiglengths)
+    nbases = contiglengths.sum()
+
+    print('', file=logfile)
+    log('Kept {} bases in {} sequences'.format(nbases, ncontigs), logfile, 1)
+    log('Processed TNF in {} seconds'.format(elapsed), logfile, 1)
+
+    return tnfs, contignames, contiglengths
 
 def run_vae_mnist():
     global vae
@@ -110,6 +238,11 @@ def run_vae_tnf_bam():
     names_path = f'{save_dir}/names.npz'
     lengths_path = f'{save_dir}/lengths.npz'
     rpkm_path = f'{save_dir}/rpkm.npz'
+    print("L")
+    with vamb.vambtools.Reader("/share_data/cami_low/CAMI_low_RL_S001__insert_270_GoldStandardAssembly.fasta", 'rb') as tnffile:
+        print("M")
+        read_contigs(tnffile, minlength=100)
+    print("Q")
     with open(f'{save_dir}/vectors.log', 'w') as logfile:
         tnf, contignames, contiglengths = calc_tnf(outdir=save_dir,
                                                     fastapath=None,
@@ -128,7 +261,8 @@ def run_vae_tnf_bam():
                             subprocesses=min(os.cpu_count(), 8), logfile=logfile)
 
     # return run_vamb_ptorch(batch_size, logfile, outdir, rpkm, tnf)
-
+    print(tnf)
+    print(rpkm)
     mask = tnf.sum(axis=1) != 0
 
     # If multiple samples, also include nonzero depth as requirement for accept
